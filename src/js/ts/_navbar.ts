5990393b5d50c6ca481ea85a2a4d430d7e0c38c7
Jonah Henriksson <33059163+JonahHenriksson@users.noreply.github.com>
2020-03-08 15:21:37 -0400

    Created HIstory Timeline chart
    I fixed the chart api so now it is easy to create charts any where, just add the cdn to the page. Also created a basic example of it on the dashboard.

--- src/js/ts/_navbar.ts
+++ src/js/ts/_navbar.ts
@@ -1,5 +1,4 @@
 export function toggleNavbar() {
-    console.log("navbar toggled");
     var navbar = document.getElementById("navbar");
     if (navbar.className === "responsive") {
         navbar.className = "";
