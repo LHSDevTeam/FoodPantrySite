452b9db78144f9e3ad12187d1e4284936d61d503
Jonah Henriksson <33059163+JonahHenriksson@users.noreply.github.com>
2020-03-09 20:08:31 -0400

    Added moment.js
    Allows for parsing of time formats, which will be useful for saving the time of user logins and events, and for formating them.

--- src/js/ts/dashboard.ts
+++ src/js/ts/dashboard.ts
@@ -1,4 +1,5 @@
 import { toggleNavbar } from "./_navbar.js";
+declare let moment: any;
 let Chart = window.Chart;
 
 document.getElementById("navbar-toggle").addEventListener("click", toggleNavbar);
@@ -6,31 +7,37 @@ document.getElementById("navbar-toggle").addEventListener("click", toggleNavbar)
 var history = new Chart("history-chart", {
     type: "line",
     data: {
-        labels: ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        labels: [ moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll"), moment().format("ll")],
         datasets: [{
             label: "Grains",
-            data: [0.5,0.4,0.3,0.6,0.8,0.2,0.2,0.1,0.3,0.4,0.6,0.2],
+            data: [0.2,0.2,0.3,0.1,0.4,0.1,0.2,0.1,0.2,0.1,0.1,0.2],
             backgroundColor: "#da7b1c",
             borderColor: "#da7b1c",
-            fill: false
+            pointBorderColor: "#FFFFFF"
         }, {
             label: "Protein",
-            data: [0.2,0.4,0.6,0.3,0.7,0.4,0.7,0.8,0.0,0.2,0.6,0.5],
+            data: [0.2,0.1,0.4,0.2,0.1,0.1,0.3,0.0,0.0,0.6,0.2,0.4],
             backgroundColor: "#6b4c9a",
             borderColor: "#6b4c9a",
-            fill: false
+            pointBorderColor: "#FFFFFF"
         }, {
             label: "Dairy",
-            data: [0.7,0.3,0.4,0.5,0.6,0.1,0.2,0.8,0.9,0.2,0.3,0.6],
+            data: [0.2,0.3,0.1,0.2,0.1,0.2,0.3,0.1,0.4,0.2,0.3,0.1],
             backgroundColor: "#3969b1",
             borderColor: "#3969b1",
-            fill: false
+            pointBorderColor: "#FFFFFF"
         }, {
             label: "Fruit",
-            data: []
+            data: [0.2,0.3,0.1,0.2,0.1,0.2,0.1,0.0,0.1,0.1,0.3,0.1],
+            backgroundColor: "#cc2528",
+            borderColor: "#cc2528",
+            pointBorderColor: "#FFFFFF"
         }, {
             label: "Vegetables",
-            data: []
+            data: [0.2,0.1,0.1,0.3,0.3,0.4,0.1,0.8,0.3,0.0,0.1,0.2],
+            backgroundColor: "#3e9651",
+            borderColor: "#3e9651",
+            pointBorderColor: "#FFFFFF"
         }]
     },
     options: {
@@ -38,8 +45,9 @@ var history = new Chart("history-chart", {
             yAxes: [{
                 ticks: {
                     beginAtZero: true,
-                    suggestedMax: 1
-                }
+                    max: 1
+                },
+                stacked: true
             }]
         }
     }
