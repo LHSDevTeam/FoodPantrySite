77a37c60a8e7bbfa3200a7cacda8d3db844168e3
Jonah Henriksson <henrikssonj@cat.pcsb.org>
2020-03-10 12:25:51 -0400

    Button needs work

--- src/js/ts/scanner.ts
+++ src/js/ts/scanner.ts
@@ -1,6 +1,6 @@
 let video : HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+    navigator.mediaDevices.getUserMedia({ video: true}).then(function (stream) {
         video.srcObject = stream;
         video.play();
     }).catch(function (error) {
