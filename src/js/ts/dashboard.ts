6b40ace24aef791c2ec292540452a44ba70d15d5
Jonah Henriksson <33059163+JonahHenriksson@users.noreply.github.com>
2020-03-09 20:13:12 -0400

    Update dashboard.ts
    Changed values

--- src/js/ts/dashboard.ts
+++ src/js/ts/dashboard.ts
@@ -16,25 +16,25 @@ var history = new Chart("history-chart", {
             pointBorderColor: "#FFFFFF"
         }, {
             label: "Protein",
-            data: [0.2,0.1,0.4,0.2,0.1,0.1,0.3,0.0,0.0,0.6,0.2,0.4],
+            data: [0.2,0.1,0.4,0.2,0.1,0.1,0.3,0.1,0.1,0.6,0.2,0.4],
             backgroundColor: "#6b4c9a",
             borderColor: "#6b4c9a",
             pointBorderColor: "#FFFFFF"
         }, {
             label: "Dairy",
-            data: [0.2,0.3,0.1,0.2,0.1,0.2,0.3,0.1,0.4,0.2,0.3,0.1],
+            data: [0.2,0.3,0.1,0.2,0.1,0.2,0.3,0.1,0.3,0.2,0.3,0.1],
             backgroundColor: "#3969b1",
             borderColor: "#3969b1",
             pointBorderColor: "#FFFFFF"
         }, {
             label: "Fruit",
-            data: [0.2,0.3,0.1,0.2,0.1,0.2,0.1,0.0,0.1,0.1,0.3,0.1],
+            data: [0.2,0.3,0.1,0.2,0.1,0.2,0.1,0.1,0.1,0.1,0.3,0.1],
             backgroundColor: "#cc2528",
             borderColor: "#cc2528",
             pointBorderColor: "#FFFFFF"
         }, {
             label: "Vegetables",
-            data: [0.2,0.1,0.1,0.3,0.3,0.4,0.1,0.8,0.3,0.0,0.1,0.2],
+            data: [0.2,0.1,0.1,0.3,0.3,0.4,0.1,0.6,0.3,0.0,0.1,0.2],
             backgroundColor: "#3e9651",
             borderColor: "#3e9651",
             pointBorderColor: "#FFFFFF"
