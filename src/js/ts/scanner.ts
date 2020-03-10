e0000187b77502940b48cdf227eed46210615b1a
Jonah Henriksson <henrikssonj@cat.pcsb.org>
2020-03-10 10:21:42 -0400

    Update scanner.ts
    Added error catching

--- src/js/ts/scanner.ts
+++ src/js/ts/scanner.ts
@@ -5,5 +5,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
         video.srcObject = stream;
         video.play();
+    }).catch(function (error) {
+      console.error(error);
     });
 }
