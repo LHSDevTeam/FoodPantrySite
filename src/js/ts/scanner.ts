89e5385c5500a00127c8d802eff6cd743336e299
Jonah Henriksson <henrikssonj@cat.pcsb.org>
2020-03-11 09:07:22 -0400

    Update scanner.ts
    Removed checker since it just doesn't work on chrome on windows. Need to check if it works on mobile devices.

--- src/js/ts/scanner.ts
+++ src/js/ts/scanner.ts
@@ -2,22 +2,7 @@ document.getElementById('stopbt').addEventListener("click",function(e) {
     console.log("Button has been pressed.");
 });
 
-console.log(navigator.mediaDevices.getSupportedConstraints());
- 
-let deviceId: string;
-navigator.mediaDevices.enumerateDevices()
-.then(function(devices) {
-  devices.forEach(function(device) {
-    if (device.label.toLowerCase().includes("world")) {
-      deviceId = device.deviceId;
-    }
-    console.log(device.label + " : " + device.deviceId + " : " + device.kind + " : " + device.groupId);
-  });
-})
-.catch(function(err) {
-  console.log(err.name + ": " + err.message);
-});
-
+// Need to check if facingMode: "environment" works on mobile devices
 let video : HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"}, audio: false}).then(function (stream) {
