04797c26d27208091187df1650d5b60a7ab7e04f
Jonah Henriksson <henrikssonj@cat.pcsb.org>
2020-03-11 05:56:52 -0400

    Updated Scanner

--- src/js/ts/scanner.ts
+++ src/js/ts/scanner.ts
@@ -1,9 +1,26 @@
 document.getElementById('stopbt').addEventListener("click",function(e) {
     console.log("Button has been pressed.");
 });
+
+console.log(navigator.mediaDevices.getSupportedConstraints());
+ 
+let deviceId: string;
+navigator.mediaDevices.enumerateDevices()
+.then(function(devices) {
+  devices.forEach(function(device) {
+    if (device.label.toLowerCase().includes("world")) {
+      deviceId = device.deviceId;
+    }
+    console.log(device.label + " : " + device.deviceId + " : " + device.kind + " : " + device.groupId);
+  });
+})
+.catch(function(err) {
+  console.log(err.name + ": " + err.message);
+});
+
 let video : HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true}).then(function (stream) {
+    navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"}, audio: false}).then(function (stream) {
         video.srcObject = stream;
         video.play();
     }).catch(function (error) {
