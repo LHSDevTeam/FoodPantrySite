15b848ee02841f7c6630fe4429240433573c7811
Jonah Henriksson <33059163+JonahHenriksson@users.noreply.github.com>
2021-05-19 13:16:06 -0400

    Added Password Encryption
    Passwords are now salted and hashed. Also added colors to info and error messages.

--- src/js/ts/main.ts
+++ src/js/ts/main.ts
@@ -14,10 +14,10 @@ async function runMongodbClient() {
   const secrets = JSON.parse(secretsFile.toString());
   mongoose.connect("mongodb+srv://" + secrets.username + ":" + secrets.password +"@web1.c3ofa.mongodb.net/jhfp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true}).then(
     () => {
-      console.log("Connected to MongoDB Database")
+      console.info('\x1b[36m%s\x1b[0m', "Connected to MongoDB Database")
     },
     err => {
-      console.log(err);
+      console.error('\x1b[31m%s\x1b[0m', err);
     }
   );
 }
@@ -85,7 +85,7 @@ function handlePOST(pathName, req, res) {
       res.end();
     } else {
       if (pathName == "/signup") {
-        POSTcallback(res, signUp(body));
+        signUp(body, res, POSTcallback);
       }
       
     }      
@@ -120,47 +120,100 @@ function userExists() {
 const UserValidationSchema = Joi.object({
   fname: Joi.string().max(36).pattern(/[a-zA-Z]/).required(),
   lname: Joi.string().max(36).pattern(/[a-zA-Z]/).required(),
-  email: Joi.string().max(254).email().required(),
+  email: Joi.string().max(256).email().required(),
+  password: Joi.string().max(128).required()
 });
+
+export interface IUser extends mongoose.Document {
+  _id: mongoose.Schema.Types.ObjectId,
+  username: String,
+  fname: String,
+  lname: String,
+  email: String,
+  hash: String
+}
 
 const UserSchema = new Schema({
   _id: Schema.Types.ObjectId,
   username: String,
   fname: String,
   lname: String,
-  email: String
+  email: String,
+  hash: String
 });
+
+
+
+UserSchema.pre<IUser>("save", function (next) {
+  const user = this;
+
+  if (this.isModified("hash") || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        console.error('\x1b[31m%s\x1b[0m', saltError);
+        return next(saltError);
+      } else {
+        bcrypt.hash(user.hash, salt, function(hashError, hash) {
+          if (hashError) {
+            console.error('\x1b[31m%s\x1b[0m', hashError);
+            return next(hashError);
+          }
+          user.hash = hash;
+          next();
+        })
+      }
+    })
+  } else {
+    return next();
+  }
+})
 
 var User = mongoose.model('Users', UserSchema);
 
 // Firsts validates sign up info and then creates a new account
-function signUp(body) {
+function signUp(body, res, callback) {
   let validation = UserValidationSchema.validate(body);
   if (validation.error) {
-    return validation;
+    console.error('\x1b[31m%s\x1b[0m', validation);
+    callback(res, validation);
   } else {
-    var newUser = new User({
-      _id: new mongoose.Types.ObjectId(),
-      username: validation.value.fname.toLowerCase() + validation.value.lname.toLowerCase(),
-      fname: validation.value.fname,
-      lname: validation.value.lname,
-      email: validation.value.email
-    });
+    User.countDocuments({username: validation.value.fname.toLowerCase() + validation.value.lname.toLowerCase()}, function (err, count){
+      if (err) {
+        let error = { error: err };
+        console.error('\x1b[31m%s\x1b[0m', error);
+        callback(res, error);
+      }
+      if (count == 0) {
+        var newUser = new User({
+          _id: new mongoose.Types.ObjectId(),
+          username: validation.value.fname.toLowerCase() + validation.value.lname.toLowerCase(),
+          fname: validation.value.fname,
+          lname: validation.value.lname,
+          email: validation.value.email,
+          hash: validation.value.password
+        });
 
-    newUser.save(function (error) {
-      if (error) {
-        let err = { error: error };
-        return err;
+        newUser.save(function (err) {
+          if (err) {
+            let error = { error: err };
+            console.error('\x1b[31m%s\x1b[0m', error);
+            callback(res, error);
+          }
+        });
+        console.trace("Sign Up Successful: " + newUser);
+        callback(res, "Sign Up Successful");
+      } else {
+        let error = {error: "Username Already Exists"}
+        console.error('\x1b[31m%s\x1b[0m', error);
+        callback(res, error);
       }
     });
-
-    return newUser;
   }
 }
 
 exports.server.listen(1337, '127.0.0.1');
 runMongodbClient();
-console.log('Server running at http://127.0.0.1:1337/');
+console.info('\x1b[36m%s\x1b[0m', "Server running at http://127.0.0.1:1337/");
 
 exports.close = function(callback: any) {
   exports.server.close(callback);
